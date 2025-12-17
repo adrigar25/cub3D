4b00358d440a490823a2a45bc86ecee721070c73
Adrián <adrigar250503@gmail.com>
2025-12-18 00:47:07 +0100

    realloc arreglado

--- libs/libft/ft_realloc.c
+++ libs/libft/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 16:45:00 by agarcia           #+#    #+#             */
-/*   Updated: 2025/12/17 16:53:45 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/12/17 22:38:51 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,41 @@
 /*
 ** FUNCIÓN: ft_realloc
 ** ------------------
-** Cambia el tamaño de la memoria apuntada por ptr a size bytes.
+** Cambia el tamaño de la memoria apuntada por ptr a new_size bytes.
 ** Si ptr es NULL, funciona como malloc.
-** Si size es 0, funciona como free y retorna NULL.
+** Si new_size es 0, funciona como free y retorna NULL.
 ** Si la reasignación falla, retorna NULL y el bloque original no se modifica.
 **
 ** PARÁMETROS:
 ** - void *ptr: Puntero a la memoria a redimensionar.
-** - size_t size: Nuevo tamaño deseado.
+** - size_t old_size: Tamaño actual del bloque de memoria.
+** - size_t new_size: Nuevo tamaño deseado.
 **
 ** RETORNO:
 ** - Un puntero a la memoria redimensionada, o NULL si falla.
 **
 */
 
-void	*ft_realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-	if (size == 0)
+	void	*new_ptr;
+	size_t	copy_size;
+
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	if (!ptr)
-		return (malloc(size));
-	return (realloc(ptr, size));
+		return (malloc(new_size));
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	if (old_size < new_size)
+		copy_size = old_size;
+	else
+		copy_size = new_size;
+	ft_memcpy(new_ptr, ptr, copy_size);
+	free(ptr);
+	return (new_ptr);
 }
