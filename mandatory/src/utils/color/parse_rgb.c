23a1e935d630e53aeb7ea83dfbe0b5577fb2a5c3
Adrián <adrigar250503@gmail.com>
2026-01-29 02:27:54 +0100

    feat: Añadir implementación del modo mandatory
    - Implementación básica del proyecto sin bonus
    - Raycasting con texturas básicas
    - Control de movimiento con teclado
    - Parseo de mapas .cub
    - Validación de mapas

---
+++ mandatory/src/utils/color/parse_rgb.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_rgb.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 15:05:47 by adriescr          #+#    #+#             */
+/*   Updated: 2026/01/24 12:45:23 by agarcia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../cub3d.h"
+
+int	parse_rgb(char *line)
+{
+	int		red;
+	int		green;
+	int		blue;
+	char	*first_comma;
+	char	*second_comma;
+
+	if (check_rgb_format(line) == -1)
+		return (-2);
+	first_comma = ft_strchr(line, ',');
+	second_comma = ft_strchr(first_comma + 1, ',');
+	if (!first_comma || !second_comma)
+		return (-2);
+	red = ft_atoi(line);
+	green = ft_atoi(first_comma + 1);
+	blue = ft_atoi(second_comma + 1);
+	if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0
+		|| blue > 255)
+		return (-2);
+	return (rgb_to_hex(red, green, blue));
+}
---
+++ mandatory/src/utils/map/check_walls.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_walls.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 16:00:00 by agarcia           #+#    #+#             */
+/*   Updated: 2026/01/24 17:44:02 by agarcia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../cub3d.h"
+
+static void	get_direction(int i, int *dx, int *dy)
+{
+	if (i == 0 || i == 4 || i == 5)
+		*dx = 1;
+	if (i == 1 || i == 6 || i == 7)
+		*dx = -1;
+	if (i == 2 || i == 4 || i == 6)
+		*dy = 1;
+	else if (i == 3 || i == 5 || i == 7)
+		*dy = -1;
+}
+
+static int	is_valid_neighbor(char **map, int x, int y, int i)
+{
+	int	new_x;
+	int	new_y;
+	int	dx;
+	int	dy;
+
+	dx = 0;
+	dy = 0;
+	get_direction(i, &dx, &dy);
+	new_x = x + dx;
+	new_y = y + dy;
+	if (new_x < 0)
+		return (0);
+	if (!map[new_x])
+		return (0);
+	if (new_y < 0)
+		return (0);
+	if (new_y >= (int)ft_strlen(map[new_x]))
+		return (0);
+	if (ft_strchr(" \0", map[new_x][new_y]))
+		return (0);
+	return (1);
+}
+
+int	check_walls(char **map)
+{
+	int	i;
+	int	x;
+	int	y;
+
+	x = -1;
+	while (map[++x])
+	{
+		y = -1;
+		while (map[x][++y])
+		{
+			if (!(map[x][y] == '0' || is_player_char(map[x][y])))
+				continue ;
+			i = 0;
+			while (i < 8)
+				if (!is_valid_neighbor(map, x, y, i++))
+					return (-1);
+		}
+	}
+	return (0);
+}
