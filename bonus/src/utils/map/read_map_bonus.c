2871bf1d427cb13ed7e85b33da6fc659e9de2d62
Adrián <adrigar250503@gmail.com>
2026-02-18 02:03:06 +0100

    bonus: show row and column of invalid character in map error

--- bonus/src/utils/map/read_map_bonus.c
+++ bonus/src/utils/map/read_map_bonus.c
@@ -6,86 +6,98 @@
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 00:10:00 by agarcia           #+#    #+#             */
-/*   Updated: 2026/02/17 01:02:06 by agarcia          ###   ########.fr       */
+/*   Updated: 2026/02/18 00:14:05 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-static int	is_allowed_char(t_game *game, char c) {
-  int i;
+static int	find_bad_col(t_game *game, char *line)
+{
+	int	i;
+	int	j;
 
-  if (!game)
-    return (0);
-  i = 0;
-  while (i < game->allowed_count) {
-    if (game->allowed_chars[i] == c)
-      return (1);
-    i++;
-  }
-  return (0);
+	i = 0;
+	while (line[i])
+	{
+		j = 0;
+		while (j < game->allowed_count && game->allowed_chars[j] != line[i])
+			j++;
+		if (j >= game->allowed_count)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
-int	is_map_line(t_game *game, char *line) {
-  int i;
-
-  if (!line || !line[0] || line[0] == '\n')
-    return (0);
-  i = 0;
-  while (line[i]) {
-    if (!is_allowed_char(game, line[i]))
-      return (0);
-    i++;
-  }
-  return (1);
+int	is_map_line(t_game *game, char *line)
+{
+	if (!line || !line[0] || line[0] == '\n')
+		return (0);
+	if (find_bad_col(game, line) >= 0)
+		return (0);
+	return (1);
 }
 
-static int	add_line(t_game *game, int index, char *line, size_t *max_width) {
-  char **new_map;
+static int	report_invalid_line(t_game *game, char *line, int index)
+{
+	int	col;
 
-  if (!is_map_line(game, line)) {
-    if (line)
-      ft_fprintf(2, RED "Error: invalid map line: %s" RESET "\n", line);
-    else
-      ft_fprintf(2, RED "Error: invalid map line: (null)" RESET "\n");
-    return (-1);
-  }
-  if (ft_strlen(line) > *max_width)
-    *max_width = ft_strlen(line);
-  new_map = ft_realloc(game->map, sizeof(char *) * index,
-                       sizeof(char *) * (index + 2));
-  if (!new_map)
-    return (-1);
-  game->map = new_map;
-  game->map[index] = ft_strtrim(line, "\n");
-  if (!game->map[index])
-    return (-1);
-  return (0);
+	col = -1;
+	if (line)
+		col = find_bad_col(game, line);
+	if (col >= 0)
+		ft_fprintf(2, RED "Error: invalid character '%c' at row %d, col %d"
+			RESET "\n", line[col], index + 1, col + 1);
+	else
+		ft_fprintf(2, RED "Error: invalid map line" RESET "\n");
+	return (-1);
 }
 
-int	read_map(t_game *game, int fd, char *first_line) {
-  char *line;
-  int i;
-  size_t max_width;
+static int	add_line(t_game *game, int index, char *line, size_t *max_width)
+{
+	char	**new_map;
 
-  i = 0;
-  max_width = 0;
-  if (!first_line)
-    return (-1);
-  line = ft_strdup(first_line);
-  free(first_line);
-  while (1) {
-    if (i > 0)
-      line = ft_get_next_line(fd);
-    if (!line)
-      break ;
-    if (add_line(game, i, line, &max_width) == -1)
-      return (free(line), -1);
-    free(line);
-    i++;
-  }
-  game->map_h = i;
-  game->map_w = (int)max_width;
-  enemy_collect_from_map(game);
-  return (0);
+	if (!is_map_line(game, line))
+		return (report_invalid_line(game, line, index));
+	if (ft_strlen(line) > *max_width)
+		*max_width = ft_strlen(line);
+	new_map = ft_realloc(game->map, sizeof(char *) * index, sizeof(char *)
+			* (index + 2));
+	if (!new_map)
+		return (-1);
+	game->map = new_map;
+	game->map[index] = ft_strtrim(line, "\n");
+	if (!game->map[index])
+		return (-1);
+	return (0);
+}
+
+int	read_map(t_game *game, int fd, char *first_line)
+{
+	char	*line;
+	int		i;
+	size_t	max_width;
+
+	i = 0;
+	max_width = 0;
+	if (!first_line)
+		return (-1);
+	line = ft_strdup(first_line);
+	free(first_line);
+	while (1)
+	{
+		if (i > 0)
+			line = ft_get_next_line(fd);
+		if (!line)
+			break ;
+		if (add_line(game, i, line, &max_width) == -1)
+			return (free(line), -1);
+		free(line);
+		i++;
+	}
+	game->map_h = i;
+	game->map_w = (int)max_width;
+	enemy_collect_from_map(game);
+	return (0);
 }
